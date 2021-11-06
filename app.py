04e39a2883ea2ff50503c6fae99dabeda6ee275e
mew123321 <mew123321@yahoo.com.tw>
2021-11-06 15:16:04 +0800

    12453

--- app.py
+++ app.py
@@ -144,11 +144,13 @@ def aqi_chart_24h():
     data = resultProxy.fetchall()
 
     aqi_list = list()
+	time_list = list()
     for item in data:
         aqi_list.append( float(item['aqi']) )
+		time_list.append( item['item'])
 		
     # plot
-    plt.plot(aqi_list)
+    plt.plot(time_list, aqi_list)
     plt.grid()
     plt.savefig('img.png')
     plt.close()
